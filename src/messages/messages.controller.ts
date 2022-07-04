1c3af6f8836d821373f473351b441038099de141
Ozan <ozancck@icloud.com>
2022-07-04 23:07:39 +0300

    Update messages.controller.ts

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
-  listMessages() { }
+  listMessages() {}
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
 }
