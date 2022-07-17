44e59d21f584bfe2c36ecafe30c828fd7fc5c466
Ozan <ozancck@icloud.com>
2022-07-17 21:22:55 +0300

    Update messages.repository.ts

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -3,12 +3,29 @@ import { readFile, writeFile } from 'fs/promises';
 export class MessagesRepository {
     async findOne(id: string) { 
         const contents = await readFile('messages.json', "utf-8"); 
-        const message = JSON.parse(contents)
+        const messages = JSON.parse(contents)
 
         return messages[id];
     }
 
-    async findAll() { }
+    async findAll() {
+        const contents = await readFile ('messages.json', "utf-8")
+        const messages = JSON.parse(contents)
 
-    async create(message: string) {}
+        return messages;
+     }
+
+    async create(message: content) {
+        const contents = await readFile ('messages.json', "utf-8")
+        const messages = JSON.parse(contents)
+
+        const id = Math.floor(Math.random()*999)
+        messages[id] = {id, content}
+
+        await writeFile("messages.json", JSON.stringify(messages))
+
+
+
+
+    }
 }
