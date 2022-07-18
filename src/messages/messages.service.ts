2eb50a9ffe991bbda725abd43c4a764b4d9f0737
Ozan <ozancck@icloud.com>
2022-07-18 14:22:57 +0300

    testing of the controller

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -3,6 +3,7 @@ import { MessagesRepository } from './messages.repository';
 export class MessagesService {
   messagesRepo: MessagesRepository;
 
+  //DO NOT USE REAL APPS
   constructor() {
     this.messagesRepo = new MessagesRepository();
   }
